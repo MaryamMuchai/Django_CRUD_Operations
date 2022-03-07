fc7695018b6b3d05f9bc7f6a377694b711fa8ff7
Maryam Muchai <maryammuchai@gmail.com>
2022-03-07 10:57:44 +0300

    create models,view and forms

--- employee_register/models.py
+++ employee_register/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    firstname= models.CharField(max_length=20)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    Contact = models.CharField(max_length=15)
+
+    def __str__(self):
+        return "%s" %(self.name)
+
+    class Meta:
+        db_table = "employee"
--- employee_register/views.py
+++ employee_register/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from employee_register.models import Employee
+from employee_register.forms import Employeeform
 
 # Create your views here.
+def emp(request):
+    if request.method=="POST":
+        form=Employeeform(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect("/show")
+            except:
+                pass
+    else:
+        form = Employeeform()
+    return render(request, 'index.html',{'form':form})
+
+def show(request):
+    employees = Employee.objects.all()
+    return render(request, 'show.html',{'employees':employees})
+
+def edit(request, id):
+    employee = Employee.objects.get(id=id)
+    return render(request, 'edit.html',{'employee':employee})
+
+def update(request,id):
+    employee = Employee.objects.get(id=id)
+    form=Employeeform(request.POST,instance=employee)
+    if form.is_valid():
+        form.save()
+        return redirect("/show")
+    return render(request, 'edit.html',{'employee':employee})
+
+def destroy(request,id):
+    employee = Employee.objects.get(id=id)
+    employee.delete()
+    return redirect("/show")
+    
+
