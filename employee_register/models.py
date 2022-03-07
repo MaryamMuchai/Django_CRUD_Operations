82c992de4a8c17765edc98546c263eb8ceb7897c
Maryam Muchai <maryammuchai@gmail.com>
2022-03-07 13:07:05 +0300

    make migrations

--- employee_register/models.py
+++ employee_register/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    firstname= models.CharField(max_length=20)
+    eid = models.CharField(max_length=20)
+    firstname= models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     email = models.EmailField()
-    Contact = models.CharField(max_length=15)
+    Contact = models.CharField(max_length=20)
 
     def __str__(self):
-        return "%s" %(self.name)
+        return "%s" %(self.firstname)
 
     class Meta:
         db_table = "employee"
--- employee_register/urls.py
+++ employee_register/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from employee_register import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('emp',views.emp),
-    path('show',views.views.show),
+    path('show',views.show),
     path('edit/<int:id>',views.edit),
     path('update/<int:id>',views.update),
     path('delete/<int:id>',views.destroy),
